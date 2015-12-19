bdb9989242de619180010518210eca0685b03d4f
John Warwick <jwarwick@gmail.com>
2015-12-19 11:40:45 -0500

    Cleared ChockABlock level

--- lib/chock_a_block.ex
+++ lib/chock_a_block.ex
@@ -6,14 +6,15 @@ defmodule ChockABlock do
   use GenServer
 
   @api_key "cbfb31ca0bd485da4e54ca12fd287a0f8cf7a234"
-  @account "HBH2453086"
-  @venue "FLTEX"
-  @stock "FIQY"
+  @account "TB56474231"
+  @venue "EPSBEX"
+  @stock "FAR"
   @quantity 100_000
+  @block_size 5_000
   @delay_ms 20
 
   defmodule State do
-    defstruct [:remaining_qty, :executed, order_id: nil]
+    defstruct [:remaining_qty, :executed, order_id: nil, target_price: 50_000]
   end
 
   ##
@@ -38,6 +39,13 @@ defmodule ChockABlock do
     Client.order_book(@venue, @stock)
   end
 
+  @doc """
+  Set the client target price
+  """
+  def set_target(pid, price) do
+    GenServer.cast(pid, {:target_price, price})
+  end
+
   ##
   ## Callbacks
 
@@ -48,6 +56,10 @@ defmodule ChockABlock do
     {:ok, %State{remaining_qty: @quantity, executed: []}, 0}
   end
 
+  def handle_cast({:target_price, price}, state) do
+    {:noreply, %State{state | target_price: price}}
+  end
+
   def handle_info(:timeout, state = %State{remaining_qty: remaining_qty}) when remaining_qty <= 0 do
     IO.inspect "Finished: #{state.executed}"
     {:stop, :normal, state}
@@ -55,8 +67,8 @@ defmodule ChockABlock do
   def handle_info(:timeout, state = %State{order_id: nil}) do
     # no active order
     {:ok, last} = last_trade
-    IO.puts "Last: #{last.lastSize}@#{last.last}"
-    {:ok, result} = buy(10000, last.last + 400)
+    # IO.puts "Last: #{last.lastSize}@#{last.last}"
+    {:ok, result} = buy(block_size(state), bid_price(last.last, state))
     {:noreply, %State{state | order_id: result.id}, @delay_ms}
   end
   def handle_info(:timeout, state = %State{order_id: id}) do
@@ -65,7 +77,7 @@ defmodule ChockABlock do
       {:noreply, state, @delay_ms}
     else
       remaining = remaining_qty(state, result.totalFilled)
-      IO.puts "Filled: #{result.totalFilled}, Remaining: #{remaining}"
+      # IO.puts "Filled: #{result.totalFilled}, Remaining: #{remaining}"
       {:noreply, %State{state | remaining_qty: remaining, order_id: nil}, @delay_ms}
     end
   end
@@ -83,11 +95,17 @@ defmodule ChockABlock do
   end
 
   defp buy(qty, price) do
-    IO.puts "Buying #{qty} @ #{price}"
-    Client.buy(@account, @venue, @stock, price, qty, :limit, @api_key)
+    # IO.puts "Buying #{qty} @ #{price}"
+    Client.buy(@account, @venue, @stock, price, qty, :ioc, @api_key)
   end
 
   defp remaining_qty(%State{remaining_qty: remaining}, nil), do: remaining
   defp remaining_qty(%State{remaining_qty: remaining}, filled), do: remaining - filled
+
+  defp block_size(%State{remaining_qty: r}) when @block_size > r, do: r
+  defp block_size(_state), do: @block_size
+
+  defp bid_price(last_bid, %State{target_price: p}) when last_bid > p, do: p
+  defp bid_price(last_bid, _state), do: last_bid
 
 end
--- lib/first_steps.ex
+++ lib/first_steps.ex
@@ -4,16 +4,16 @@ defmodule FirstSteps do
   """
 
   @api_key "cbfb31ca0bd485da4e54ca12fd287a0f8cf7a234"
-  @venue "YHWEX"
-  @stock "BSLM"
-  @account "SHP88072110"
+  @venue "SOKYEX"
+  @stock "UFYP"
+  @account "JB47891558"
   @quantity 100
 
   @doc """
   Buy 100 shares of the stock at the specified price using immediate-or-cancel order type
   """
   def buy(price) do
-    Client.buy(@account, @venue, @stock, price, @quantity, :ioc, @api_key)
+    Client.buy(@account, @venue, @stock, price, @quantity, :limit, @api_key)
   end
 
 end
