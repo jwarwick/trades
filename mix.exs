7321bbf6eb7445e5d723220c625e857286a0f13a
John Warwick <jwarwick@gmail.com>
2015-12-19 20:55:33 -0500

    Handling quote websocket

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Trades.Mixfile do
   defp deps do
     [
       {:hackney, "~> 1.4"},
-      {:poison, "~> 1.5"}
+      {:poison, "~> 1.5"},
+      {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git"}
     ]
   end
 end
