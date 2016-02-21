2155866d83cc4cc31e04cb03dde9aac1850910e9
John Warwick <jwarwick@gmail.com>
2016-02-20 21:56:32 -0500

    added license

--- lib/level/first_steps.ex
+++ lib/level/first_steps.ex
@@ -8,10 +8,10 @@ defmodule Level.FirstSteps do
   """
   def execute do
     {:ok, start_data} = GameMaster.start_level(:first_steps)
-    Apex.ap start_data
+    Apex.ap start_data, numbers: false
     stock = GameMaster.make_stock_id(start_data)
-    {:ok, buy_data} = Client.buy(stock, %{qty: 100, price: 0, order_type: :market})
-    Apex.ap buy_data
+    {:ok, buy_data} = Client.buy(stock, %{qty: 100, price: 100000, order_type: :limit})
+    Apex.ap buy_data, numbers: false
     start_data |> GameMaster.instance_id |> GameMaster.status
     {:ok, GameMaster.instance_id(start_data)}
   end
